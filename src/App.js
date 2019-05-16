eb2a2c53eb87e6e6931ff5c04abfea18306f1ef2
Linus Falck-Ytter <lifayt@gmail.com>
2019-05-16 09:08:59 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
+import "./App.css";
+const FORMATSTRING = "dddd, MMMM D, YYYY h:mm:ss A";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    date: dayjs(
+      new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
+    ).format(FORMATSTRING),
+    datePlusFortyFive: dayjs(
+      new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
+    )
+      .add(45, "days")
+      .format(FORMATSTRING),
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.setState({
+      date: dayjs(
+        new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
+      ).format(FORMATSTRING),
+      datePlusFortyFive: dayjs(
+        new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
+      )
+        .add(45, "days")
+        .format(FORMATSTRING),
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Is this a hard thing to program?</h1>
+        <h5>Today's Date: </h5>
+        <div>{`${this.state.date}`}</div>
+        <h5>Today's Date Plus 45: </h5>
+        <div>{`${this.state.datePlusFortyFive}`}</div>
+        <div
+          style={{
+            marginTop: "3rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Copyright by <b>Linus</b>, who says it's not hard to program.
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
